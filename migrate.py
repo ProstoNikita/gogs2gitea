b1e0ea63228f5ef0b3f46f8e7f1bb133995dd8a5
nikita.iryupin <nikita.iryupin@scalesoft.cz>
2021-09-16 11:29:14 +0200

    Continue to previous commit

--- migrate.py
+++ migrate.py
@@ -396,11 +396,12 @@ def copy_user_repos(src_url, src_token, src_user, dst_url, dst_token, dst_user):
 Copies all the repositories of given organization on source server 
 to the given organization of destination server.
 """
-def copy_repos(src_url, src_token, src_org, dst_url, dst_token, dst_org):
-    repos = get_repos(src_url, src_token, src_org)
-    for repo in repos:
-        repoName = repo['name']
-        copy_repo(src_url, src_token, src_org, dst_url, dst_token, dst_org, repoName) 
+# Commented because of lack of overloading in Python
+# def copy_repos(src_url, src_token, src_org, dst_url, dst_token, dst_org):
+#     repos = get_repos(src_url, src_token, src_org)
+#     for repo in repos:
+#         repoName = repo['name']
+#         copy_repo(src_url, src_token, src_org, dst_url, dst_token, dst_org, repoName) 
 
 def print_repos(repos):
     for repo in repos:
